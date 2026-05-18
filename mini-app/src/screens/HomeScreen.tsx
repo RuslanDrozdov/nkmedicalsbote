7d3970227f20b890ffc4f58e2dae61dd7dd80a81
руслан <drozdovruslan821@gmail.com>
2026-05-18 22:59:14 +0300

    добавление 20

--- mini-app/src/screens/HomeScreen.tsx
+++ mini-app/src/screens/HomeScreen.tsx
@@ -1,12 +1,4 @@
-import type { KeyboardEvent } from "react";
-import brainBg from "../assets/brain-background.png";
-import {
-  BRAIN_VIEWBOX,
-  settingsPath,
-  settingsLabelAnchor,
-  surveyPath,
-  surveyLabelAnchor,
-} from "../assets/brainZones.generated";
+import KnowledgeGardenScene from "../components/home/KnowledgeGardenScene";
 
 type Props = {
   title: string;
@@ -18,13 +10,6 @@ type Props = {
   settingsAriaLabel: string;
 };
 
-function activateOnKey(e: KeyboardEvent, action: () => void) {
-  if (e.key === "Enter" || e.key === " ") {
-    e.preventDefault();
-    action();
-  }
-}
-
 export default function HomeScreen({
   title,
   onSurvey,
@@ -35,49 +20,17 @@ export default function HomeScreen({
   settingsAriaLabel,
 }: Props) {
   return (
-    <div className="home" style={{ backgroundImage: `url(${brainBg})` }}>
+    <div className="home">
       <header className="home-header">{title}</header>
-      <svg
-        className="home-map"
-        viewBox={`0 0 ${BRAIN_VIEWBOX.width} ${BRAIN_VIEWBOX.height}`}
-        preserveAspectRatio="xMidYMid slice"
-      >
-        <path
-          className="home-lobe home-lobe--survey"
-          d={surveyPath}
-          role="button"
-          tabIndex={0}
-          aria-label={surveyLabel}
-          onClick={onSurvey}
-          onKeyDown={(e) => activateOnKey(e, onSurvey)}
-        />
-        <path
-          className="home-lobe home-lobe--settings"
-          d={settingsPath}
-          role="button"
-          tabIndex={0}
-          aria-label={settingsAriaLabel}
-          onClick={onSettings}
-          onKeyDown={(e) => activateOnKey(e, onSettings)}
-        />
-      </svg>
+      <KnowledgeGardenScene
+        onSurvey={onSurvey}
+        onSettings={onSettings}
+        surveyAriaLabel={surveyLabel}
+        settingsAriaLabel={settingsAriaLabel}
+      />
       <div className="home-labels">
-        <span
-          className="home-label home-label--survey"
-          style={{
-            left: `${surveyLabelAnchor.x * 100}%`,
-            top: `${surveyLabelAnchor.y * 100}%`,
-          }}
-        >
-          {surveyLabel}
-        </span>
-        <span
-          className="home-label home-label--settings"
-          style={{
-            left: `${settingsLabelAnchor.x * 100}%`,
-            top: `${settingsLabelAnchor.y * 100}%`,
-          }}
-        >
+        <span className="home-label home-label--survey">{surveyLabel}</span>
+        <span className="home-label home-label--settings">
           <span>{settingsLine1}</span>
           <span>{settingsLine2}</span>
         </span>
